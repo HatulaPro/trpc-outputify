f9536afa20bade3803221a28e4a3660520c5b24c
HatulaPro <shalev280804@gmail.com>
2023-03-25 17:20:31 +0300

    Adding tests

--- src/types.test.ts
+++ src/types.test.ts
@@ -1,6 +1,6 @@
 import { Project, SyntaxKind } from 'ts-morph';
 import { describe, expect, it } from 'vitest';
-import { removePromiseFromType } from './types';
+import { getValueOfBooleanLiteral, removePromiseFromType } from './types';
 
 describe('removePromiseFromType should turn Promise<T> into T', () => {
 	const project = new Project({
@@ -54,5 +54,29 @@ describe('removePromiseFromType should turn Promise<T> into T', () => {
 		const unionTypes = resultType.getUnionTypes().map((x) => x.getText());
 		expect(unionTypes).toContain('string');
 		expect(unionTypes).toContain('number');
+	});
+});
+
+describe('getValueOfBooleanLiteral should work', () => {
+	const project = new Project({
+		compilerOptions: {
+			tsConfigFilePath: './tsconfig.json',
+		},
+	});
+	it('should return true', () => {
+		const f = project.createSourceFile(
+			'newfile.ts',
+			`const x = true as const;
+			let y = false as const;`,
+			{ overwrite: true }
+		);
+
+		const types = f.getDescendantsOfKind(SyntaxKind.VariableDeclaration);
+		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+		const literalTrue = getValueOfBooleanLiteral(types[0]!.getType());
+		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+		const literalFalse = getValueOfBooleanLiteral(types[1]!.getType());
+		expect(literalTrue).toBe(true);
+		expect(literalFalse).toBe(false);
 	});
 });
